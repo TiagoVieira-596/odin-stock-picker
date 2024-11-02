3f549722bfba84a1a09691991b956018c23d3972
Tiago <tvhollowknight@gmail.com>
2024-11-02 00:58:55 -0300

    Program now works with all cases and has better readability
    Now the user can correctly pick the best days to buy/sell

--- stockpicker.rb
+++ stockpicker.rb
@@ -1,32 +1,18 @@
 def stock_picker (stocks_array)
-  days_in_comparision = [0, 0]
-  best_days_found = false
+  best_days = [0, 0]
   max_profit = 0
-
-  until best_days_found == true or max_profit == (stocks_array.max - stocks_array.min)
-    min_value = stocks_array.filter {|stock| stock > days_in_comparision[0]}.min
-    if min_value == nil
-      best_days_found = true
-      break
+  stocks_array.each_with_index do |buy_price, buy_day|
+    stocks_array.each_with_index do |sell_price, sell_day|
+      if buy_day < sell_day
+        profit = sell_price - buy_price
+        if profit > max_profit
+          max_profit = profit
+          best_days = [buy_day, sell_day]
+        end
+      end
     end
-    max_value_after_min = stocks_array.filter {|stock| stocks_array.find_index(stock) > stocks_array.find_index(min_value)}.max
-    if max_value_after_min == nil or min_value == nil
-      best_days_found = true
-      break
-    else
-      days_in_comparision = [min_value, max_value_after_min]
-    end
-    if days_in_comparision[1] - days_in_comparision[0] > max_profit
-      max_profit = days_in_comparision[1] - days_in_comparision[0]
-      current_best_days = days_in_comparision
-    end
-    days_in_comparision
   end
-  days_chosen = [stocks_array.find_index(current_best_days[0]), stocks_array.find_index(current_best_days[1])]
-  if days_chosen[0] > days_chosen[1]
-    return "Not possible to get any profit"
-  end
-  days_chosen
+  best_days
 end
 
-p stock_picker([1, 100, 80])
+p stock_picker([10, 9, 45])
